77b911d81d544ebd43106893e7b6eb8345655508
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2018-09-22 22:29:06 +0200

    Change the way tasks handle steps

--- src/task/BaseTask/BaseTask.ts
+++ src/task/BaseTask/BaseTask.ts
@@ -1,7 +1,6 @@
 import { ISearchItemData, SearchItem } from './step/SearchItem';
 import { Bot } from '../../Bot';
 import { Step } from './step/Step';
-import { PaymentStep } from './step/payment/PaymentStep';
 import { IProxy } from '@util/proxy';
 import { log } from '@util/log';
 import { IProductData, ITaskData } from '../../config/ITasksConfig';
@@ -12,9 +11,13 @@ import { compensateInterval } from '@util/timing';
 
 let taskId = 0;
 
+export enum StepBreakpoint {
+    ProductsBreakpoint = 0,
+    PaymentsBreakpoint,
+}
+
 export abstract class BaseTask {
     public id: number;
-    public lastReRun: number;
     public mainUrl: string;
     public isMonitoring: boolean;
     public startDelay: number;
@@ -25,11 +28,13 @@ export abstract class BaseTask {
     public account: IAccountData;
     public order: Order;
     public products: IProductData[];
-    public orders: Order[];
     public searchItems: SearchItem[] = [];
 
-    private finishedInit = false;
-    private waitingToStart = true;
+    public firstRun = 0;
+    public lastRun = 0;
+    public lastReRun = 0;
+
+    private hasStarted = false;
 
     constructor(public bot: Bot, taskData: ITaskData, startInit = true) {
         this.id = taskId++;
@@ -44,7 +49,7 @@ export abstract class BaseTask {
         this.startDelay = this.isMonitoring ? baseData.monitoring.startDelay : 0;
         this.mainUrl = `http://www.${baseData.store.domainsByRegion[baseData.storeRegion]}`;
         this.interval = baseData.interval;
-        this.orders = baseData.orders;
+        this.order = baseData.order;
         this.startTime = baseData.startTime;
         this.products = baseData.products;
 
@@ -63,97 +68,53 @@ export abstract class BaseTask {
     }
 
     protected startInit(): void {
-        this.init(this.doneInit);
+        this.init(() => this.doneInit());
     }
 
     protected doneInit(): void {
-        this.finishedInit = true;
         this.log('Finished initialisation');
-        if(this.waitingToStart) {
+
+        setTimeout(() => {
             compensateInterval(() => {
                 this.log('Starting task');
                 this.run();
             }, this.startTime - Date.now());
-        }
+        });
     }
 
-    protected abstract getSearchItemClassReference(): { new(data: ISearchItemData): SearchItem };
     protected abstract init(done?: () => void): void;
-    protected abstract getStepClassReferences(): { new(): Step }[];
-    protected abstract getInsertPaymentClassesAfterClassReference(): { new(): PaymentStep };
-    protected abstract getPaymentClassReferences(): { new(): PaymentStep }[];
 
-    async run(startStepClassReference: { new(): Step } = null): Promise<void> {
-        if(!this.finishedInit) {
-            this.waitingToStart = true;
-            return;
+    protected abstract getSearchItemClassReference(): { new(data: ISearchItemData): SearchItem };
+
+    protected abstract getSteps(): ({ new(...args: any[]): Step } | StepBreakpoint)[];
+    protected abstract getProductSteps(): { new(...args: any[]): Step }[];
+    protected abstract getPaymentSteps(): { new(...args: any[]): Step }[];
+
+    protected getStepsByBreakpoint(): { [breakpoint: number]: { new(...args: any[]): Step }[] } {
+        const stepsByBreakpoint: { [breakpoint: number]: { new(...args: any[]): Step }[] } = {};
+        stepsByBreakpoint[StepBreakpoint.PaymentsBreakpoint] = this.getPaymentSteps();
+        stepsByBreakpoint[StepBreakpoint.ProductsBreakpoint] = this.getProductSteps();
+
+        return stepsByBreakpoint;
+    }
+
+    async run(): Promise<void> {
+        if(!this.hasStarted) {
+            this.firstRun = Date.now();
+            this.hasStarted = true;
+        }else {
+            this.lastRun = Date.now();
         }
 
-        this.lastReRun = Date.now();
+        const firstStep = this.getSteps()[0];
+        const stepsByBreakpoint = this.getStepsByBreakpoint();
+        if(Number.isInteger(<number> firstStep)) { // is breakpoint
+            const breakpointStep = stepsByBreakpoint[<number> firstStep][0];
 
-        const stepClassReferences = this.getStepClassReferences();
-        const insertPaymentClassesAfterClassReference = this.getInsertPaymentClassesAfterClassReference();
-        const paymentClassReferences = this.getPaymentClassReferences();
-
-        const previousStepFunction = (currentStepClassReference: { new(...args: any[]): Step }, resultsByClassReference: any, previousStepClassReference: { new(...args: any[]): Step }): void => {
-            this.lastReRun = Date.now();
-
-            const realPreviousStepClassReference: { new(...args: any[]): Step } = previousStepClassReference === undefined ? (stepClassReferences[stepClassReferences.indexOf(currentStepClassReference) - 1] || stepClassReferences[0]) : previousStepClassReference;
-            const previousStepClassReferenceIndex: number = stepClassReferences.indexOf(previousStepClassReference);
-
-            let index: number = 0;
-            for(const stepClassReference in resultsByClassReference) {
-                if(index++ >= previousStepClassReferenceIndex) {
-                    delete resultsByClassReference[stepClassReference];
-                }
-            }
-
-            this.log(realPreviousStepClassReference.name);
-            new realPreviousStepClassReference(self, previousStepFunction, nextStepFunction, resultsByClassReference).run();
-        };
-
-        const nextStepFunction = (currentStepClassReference: { new(...args: any[]): Step }, proxy: IProxy, resultsByClassReference: any, currentStepResult: any): void => {
-            let nextStepClassReference: { new(...args: any[]): Step } = null;
-            resultsByClassReference[<any> currentStepClassReference] = currentStepResult; // fix <any> later
-
-            const values: any[] = Object.values(resultsByClassReference);
-            const results: any = values.length === 0 ? {} : Object.assign({}, ...values);
-            const currentStepClassReferenceIndex = stepClassReferences.indexOf(currentStepClassReference);
-
-            if(currentStepClassReference.name === insertPaymentClassesAfterClassReference.name) {
-                const paymentMethod: string = results.order.payment.method;
-                const newPaymentClassReferences: { new(...args: any[]): Step }[] = paymentClassReferences[paymentMethod];
-
-                if(newPaymentClassReferences.length === 0) {
-                    nextStepClassReference = stepClassReferences[currentStepClassReferenceIndex + 1];
-                }else {
-                    nextStepClassReference = newPaymentClassReferences[0];
-                }
-            }else {
-                if(currentStepClassReferenceIndex === -1) {
-                    // probably payment class
-                    const paymentMethod: string = results.order.payment.method;
-                    const newPaymentClassReferences: any[] = paymentClassReferences[paymentMethod];
-
-                    nextStepClassReference = newPaymentClassReferences[newPaymentClassReferences.indexOf(currentStepClassReference) + 1];
-
-                    if(nextStepClassReference === undefined) { // current step class is the last payment class
-                        nextStepClassReference = stepClassReferences[stepClassReferences.indexOf(insertPaymentClassesAfterClassReference) + 1];
-                    }
-                }else {
-                    nextStepClassReference = stepClassReferences[currentStepClassReferenceIndex + 1];
-                }
-            }
-            if(nextStepClassReference === undefined) {
-                return;
-            }
-            this.log(nextStepClassReference.name);
-            new nextStepClassReference(this, proxy, previousStepFunction, nextStepFunction, resultsByClassReference).run();
-        };
-
-        const firstStepClassReference: { new(...args: any[]): Step } = stepClassReferences[startStepClassReference === null ? 0 : stepClassReferences.indexOf(startStepClassReference)];
-        this.log(firstStepClassReference.name);
-        new firstStepClassReference(this, this.proxy, previousStepFunction, nextStepFunction, {}).run();
+            new breakpointStep(this, this.mainProxy, {}, [0, 0], this.getSteps(), stepsByBreakpoint);
+        }else {
+            new (<{ new(...args: any[]): Step }> firstStep)(this, this.mainProxy, {}, [0, 0], this.getSteps(), stepsByBreakpoint);
+        }
     }
 
     log(string: string, file?: string): void {
--- src/task/BaseTask/step/Step.ts
+++ src/task/BaseTask/step/Step.ts
@@ -1,4 +1,4 @@
-import { BaseTask } from '../BaseTask';
+import { BaseTask, StepBreakpoint } from '../BaseTask';
 import { IProxy } from '@util/proxy';
 import { IRequestOptions, IResponse, makeRequest, RequestMethod } from '@util/request';
 
@@ -7,28 +7,87 @@ export interface IStepResults {
 }
 
 export abstract class Step {
-    protected task: BaseTask;
-    protected proxy: IProxy;
-    protected previousStep: (previousStepClass: { new(...args: any[]): Step }) => void;
-    protected nextStep: (stepResult?: any, proxy?: IProxy) => void;
     protected results: any;
 
-    constructor(task: BaseTask, proxy: IProxy = null, previousStep: (...args: any[]) => void, nextStep: (...args: any[]) => void, resultsByClass: any) {
-        this.task = task;
-        this.proxy = proxy;
-        this.previousStep = (previousStepClass: { new(...args: any[]): Step }): void => {
-            previousStep(this.getReference(), resultsByClass, previousStepClass);
-        };
-        this.nextStep = (stepResult: any = {}, proxy: IProxy = null) => {
-            nextStep(
-                this.getReference(),
-                proxy,
-                resultsByClass,
-                stepResult,
-            );
-        };
-        const values: any[] = Object.values(resultsByClass);
-        this.results = values.length === 0 ? {} : Object.assign({}, ...values);
+    constructor(
+        protected task: BaseTask,
+        protected proxy: IProxy = null,
+        private resultsByStepIndex: any = {},
+        private stepIndex: [number, number] = [0, 0],
+        protected steps: ({ new(...args: any[]): Step } | StepBreakpoint)[] = [],
+        protected stepsByBreakpoint: { [breakpoint: number]: { new(...args: any[]): Step }[] },
+    ) {
+        this.results = this.resultsByStepIndex.length === 0 ? {} : Object.assign({}, ...Object.values(this.resultsByStepIndex));
+    }
+
+    protected nextStep(result: { [x: string]: any }) {
+        if(this.stepIndex[0] === this.steps.length - 1) {
+            if(!Array.isArray(this.stepIndex[this.steps.length - 1])) {
+                return;
+            }
+            if(this.stepIndex[1] === (<{ new(...args: any[]): Step }> this.steps[this.stepIndex[0]]).length - 1) {
+                return;
+            }
+        }
+
+        this.resultsByStepIndex[this.stepIndex[0]] = result;
+
+        const step = this.steps[this.stepIndex[0] - 1];
+        if(Array.isArray(step)) {
+            const breakpointSteps = this.stepsByBreakpoint[<StepBreakpoint> step];
+
+            new breakpointSteps[0](this.task, this.proxy, this.resultsByStepIndex, [this.stepIndex[0] + 1, 0], this.steps, this.stepsByBreakpoint).run();
+        }else {
+            new (<{ new(...args: any[]): Step }> step)(this.task, this.proxy, this.resultsByStepIndex, [this.stepIndex[0] + 1, 0], this.steps, this.stepsByBreakpoint).run();
+        }
+    }
+
+    protected previousStep(previousStep?: { new(...args: any[]): Step }) {
+        if(this.stepIndex[0] === 0) {
+            return;
+        }
+
+        if(previousStep) {
+            let previousStepIndex = this.steps.indexOf(previousStep);
+            let previousStepSubIndex = 0;
+
+            if(previousStepIndex === -1) {
+                let stepIndex = 0;
+
+                for(const breakpoint of this.steps) {
+                    stepIndex++;
+
+                    if(!Number.isInteger(<StepBreakpoint> breakpoint)) {
+                        continue;
+                    }
+
+                    const breakpointSteps = this.stepsByBreakpoint[<StepBreakpoint> breakpoint];
+                    previousStepSubIndex = breakpointSteps.indexOf(previousStep);
+
+                    if(previousStepSubIndex !== -1) {
+                        previousStepIndex = stepIndex;
+                    }
+                }
+            }
+
+            if(previousStepIndex === -1) {
+                throw new Error('Previous step does not exist');
+            }
+
+            new previousStep(this.task, this.proxy, this.resultsByStepIndex, [previousStepIndex, previousStepSubIndex], this.steps, this.stepsByBreakpoint).run();
+            return;
+        }
+
+        const step = this.steps[this.stepIndex[0] - 1];
+        if(Array.isArray(step)) {
+            const breakpointSteps = this.stepsByBreakpoint[<StepBreakpoint> step];
+
+            const breakpointStepIndex = 0; // or should this be the last step?
+
+            new breakpointSteps[breakpointStepIndex](this.task, this.proxy, this.resultsByStepIndex, [this.stepIndex[0] - 1, breakpointStepIndex], this.steps, this.stepsByBreakpoint).run();
+        }else {
+            new (<{ new(...args: any[]): Step }> step)(this.task, this.proxy, this.resultsByStepIndex, [this.stepIndex[0] - 1, 0], this.steps, this.stepsByBreakpoint).run();
+        }
     }
 
     protected log(string: string) {
@@ -40,11 +99,13 @@ export abstract class Step {
             if(options.proxy === undefined || options.proxy === null) {
                 options.proxy = this.proxy;
             }
+
             return await makeRequest(method, url, options);
         }catch(error) {
             if(this.task.bot.isUsingDeveloperMode) {
-                console.log(error);
+                console.error(error);
             }
+
             throw error;
         }
     }
@@ -52,14 +113,15 @@ export abstract class Step {
     abstract getReference(): { new(...args: any[]): Step };
     abstract run(): void;
 
-    reRun(classReference?: { new(...args: any[]): Step }, now: boolean = false): void {
+    reRun(step?: { new(...args: any[]): Step }, now: boolean = false): void {
         const reRun = (): void => {
-            if(classReference === undefined || classReference === null) {
-                this.previousStep(this.getReference());
+            if(step === undefined || step === null) {
+                this.previousStep();
             }else {
-                this.previousStep(classReference);
+                this.previousStep(step);
             }
         };
+
         if(now) {
             reRun();
         }else {
