6b97fa6614c9597478bc188a8375b66c31e1c0ef
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2018-10-07 00:12:23 +0200

    Create ISession for browser sessions

---
+++ src/session/ISession.ts
@@ -0,0 +1,17 @@
+import { IRequestOptions, IResponse, RequestMethod } from '@util/request';
+
+export interface ICookie {
+    name: string;
+    value: string;
+    path: string;
+    domain: string;
+    secure: boolean;
+    httpOnly: boolean;
+    sameSite: string;
+}
+
+export interface ISession {
+    url: string;
+    cookies: ICookie[];
+    makeRequest: (method: RequestMethod, url: string, options: IRequestOptions) => Promise<IResponse>;
+}
