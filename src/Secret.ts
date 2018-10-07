98da06369f54a642b7eb1becafd72d8a75497eb4
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2018-10-07 17:01:56 +0200

    Fix broken secret

--- src/Secret.ts
+++ src/Secret.ts
@@ -1,17 +1,15 @@
 import { existsSync, readFileSync, writeFileSync } from 'fs';
 import { randomBytes } from 'crypto';
 
-const path: string = __dirname + '/../.secret';
+const path = `${__dirname}/../.secret`;
 
-let _secret: string;
+let secret = null;
 
 if(existsSync(path)) {
-    _secret = readFileSync(path, 'utf8');
+    secret = readFileSync(path, 'utf8');
 }else {
-    _secret = randomBytes(128).toString('hex');
-    writeFileSync(path, _secret, 'utf8');
+    secret = randomBytes(128).toString('hex');
+    writeFileSync(path, secret, 'utf8');
 }
 
-secret = _secret;
-
-export let secret;
+export { secret };
