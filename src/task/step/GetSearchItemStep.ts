31f341efe42737b4686bf3f2db42e915042a70f8
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2018-11-01 14:56:59 +0100

    New GetSearchItemStep (first step towards rewriting all the step classes)

--- src/task/step/GetSearchItemStep.ts
+++ src/task/step/GetSearchItemStep.ts
@@ -1,77 +1,44 @@
-import { IStepResults, Step } from './Step';
 import { SearchItem } from './SearchItem';
 import { containsAnArrayItem, containsArrayItems } from '@util/generic';
 import { compensateInterval, ICompensateInterval } from '@util/timing';
-import { BaseTask } from '../BaseTask';
-import { StepIndex, StepResult } from '../../manager/StepManager';
+import { Step } from './Step';
+import { ProductConfigData } from '../../config/ITasksConfig';
 
-export interface IGetSearchItemStepResults extends IStepResults {
-    searchItem: SearchItem;
-}
-
-export abstract class GetSearchItemStep extends Step {
-    protected task: Task;
+export abstract class GetSearchItemStep extends Step<{}, { product: ProductConfigData }> {
     protected interval: ICompensateInterval;
 
-    constructor(task: BaseTask, stepIndex: StepIndex, results: StepResult) {
-        super(task, stepIndex, results);
-        this.interval = compensateInterval(() => {
-            this.run();
-        }, this.task.interval, false);
+    run(): void {
+        this.interval = compensateInterval(async () => {
+            const searchItem = this.findSearchItem(await this.getSearchItems(this.results.product.search));
+
+            if(searchItem !== null) {
+                this.log(`Found search item with name ${searchItem.name} and URL ${searchItem.url}`);
+                this.task.searchItemsByName[searchItem.name] = searchItem;
+                this.interval.stop();
+                this.nextStep();
+                return;
+            }
+            this.log('Could not find search item');
+        }, this.task.interval, true);
     }
 
-    protected foundSearchItem(searchItem: SearchItem): void {
-        if(this.task.isMonitoring) {
-            this.log('Will start looking for sizes at start time');
-            setTimeout(() => {
-                this.nextStep({ searchItem: searchItem });
-            }, this.task.startTime ? this.task.startTime - Date.now() + this.task.startDelay : this.task.startDelay);
-        }else {
-            this.nextStep({ searchItem: searchItem });
-        }
-    }
+    protected abstract async getSearchItems(search: string): Promise<SearchItem[]>;
 
-    async run(): Promise<void> {
-        if(this.task.searchItem !== null) {
-            this.interval.stop();
-            this.foundSearchItem(this.task.searchItem);
-        }else {
-            try {
-                const searchItems = await this.getSearchItems(this.task.product.search);
-                if(searchItems === undefined) { // then the child should use the trySearchItem method manually
-                    return;
-                }
-                for(let i = 0; i < searchItems.length; i++) {
-                    const searchItem = searchItems[i];
-                    if(this.trySearchItem(searchItem)) {
-                        return;
-                    }
-                }
-                this.log('Could not find search item');
-            }catch(error) {
-                if(error.name === 'StatusCodeError') {
-                    this.log(`Could not get search items: status code ${error.statusCode}`);
-                }else {
-                    this.log(`Could not get search items: ${error.message}`);
-                }
+    private findSearchItem(searchItems: SearchItem[]): SearchItem {
+        for(const searchItem of searchItems) {
+            if(this.isValidSearchItem(searchItem)) {
+                this.task.searchItemsByName[searchItem.name] = searchItem;
+                return searchItem;
             }
         }
+
+        return null;
     }
 
-    protected isValidSearchItem(searchItem: SearchItem): boolean {
-        return containsArrayItems(searchItem.name, this.task.product.filter.title.contains) && !containsAnArrayItem(searchItem.name, this.task.product.filter.title.blocked);
+    private isValidSearchItem(searchItem: SearchItem): boolean {
+        return containsArrayItems(searchItem.name, this.results.product.filter.name.contains)
+            && !containsAnArrayItem(searchItem.name, this.results.product.filter.name.blocked)
+            && containsArrayItems(searchItem.url, this.results.product.filter.url.contains)
+            && !containsAnArrayItem(searchItem.url, this.results.product.filter.url.blocked);
     }
-
-    protected trySearchItem(searchItem: SearchItem): boolean {
-        if(this.isValidSearchItem(searchItem)) {
-            this.log(`Found search item with name ${searchItem.name} and URL ${searchItem.url}`);
-            this.task.searchItem = searchItem;
-            this.interval.stop();
-            this.foundSearchItem(searchItem);
-            return true;
-        }
-        return false;
-    }
-
-    protected abstract async getSearchItems(searchValue: string): Promise<SearchItem[]>;
 }
