d120727222aee8097f690746b5b495bb0f4497b4
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2018-11-01 15:12:26 +0100

    Init method for Step class

---
+++ src/task/step/Step.ts
@@ -0,0 +1,45 @@
+import { BaseTask } from '../BaseTask';
+import { IRequestOptions, IResponse, makeRequest, RequestMethod } from '@util/request';
+import { StepIndex, StepResult } from '../../manager/StepManager';
+
+export abstract class Step<ResultType extends object = StepResult, CurrentResultsType extends object = StepResult> {
+    constructor(
+        protected task: BaseTask,
+        private stepIndex: StepIndex,
+        protected results: CurrentResultsType,
+    ) {
+        this.init();
+    }
+
+    protected init(): void {}
+
+    protected log(string: string): void {
+        this.task.log(string);
+    }
+
+    protected async makeRequest(method: RequestMethod, url: string, options: IRequestOptions = {}): Promise<IResponse> {
+        try {
+            if(options.proxy === undefined) {
+                options.proxy = this.task.mainProxy;
+            }
+
+            return await makeRequest(method, url, options);
+        }catch(error) {
+            if(this.task.bot.isUsingDeveloperMode) {
+                console.error(error);
+            }
+
+            throw error;
+        }
+    }
+
+    abstract run(): void;
+
+    nextStep(result: StepResult = {}): void {
+        this.task.stepManager.nextStep(this.stepIndex, result);
+    }
+
+    previousStep(): void {}
+
+    reRun(): void {}
+}
